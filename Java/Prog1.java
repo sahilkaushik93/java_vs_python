64baacdf47c15ee285092e9b4fcec92589fae588
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-10-26 22:05:31 +0530

    Starting Java Learning - v2.0. - Lesson 1

---
+++ Java/Prog1.java
@@ -0,0 +1,32 @@
+/*
+ * To write a code in Java:-
+ * 1. In Java first we create a class
+ * 2. Then we create a function in it
+ * 3. In that function we transmute psuedo code into logical form
+ * 4. To save file make sure you are saving it with the same name you have created main class of program. 
+ * 
+ * How your code is running ?
+ * JDK(Java Dev. Kit) = JRE(Java run env.) + Development Tools
+ * JRE = JVM(Java virtual Machine) + Libraries
+ * 1. COMPILATION:
+ * Source Code(ext: Prog1.java) -> Compiler(In JDK) -> Byte Code(ext: xyz.class) 
+ * This Byte Code can run on any machine provided JRE is present.
+ * 2. EXECUTION:
+ * Byte Code -> JVM -> native code(code that machine understands)
+ * 
+ * Code Components:
+ * 1. Function: void main(){}
+ * Used to perform some operations, required as per psuedo code
+ * 2. Class: Class class_name{void Function1(){} void Function2(){}.. so on}
+ * Entity that can contain more than one function
+ * 'Main()' function is executed first then rest of the functions sequentially.
+ * 
+*/
+
+ class Prog1 {
+    public static void main(String args[]) {
+        System.out.println("Hello World, It's my version 2.0. of Java Practise !!");
+    }
+ }
+
+ 
