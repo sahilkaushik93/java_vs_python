09335ebb6e1fea77499fc31520e5dbd67742a634
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-10-30 17:44:46 +0530

    Java Learning - Day 2

---
+++ Java/Prog2.java
@@ -0,0 +1,63 @@
+/*
+ * How to give Output ?
+ * How to define variables ?
+ * How to define data types ?
+ * How to take Input ?
+ */
+
+public class Prog2 {
+    public static void main(String[] args) {
+    
+    // PRINTING OUTPUT
+    /*
+     * "System": It is a class, that contains some objects.
+     * 
+     * "System.out.print":
+     * By Default: 'print' function doesn't provide feature of next line
+     * If want to print next statement in next line use "\n".
+     * 
+     * "System.out.println":
+     * By Default: 'println' function provides feature of next line.
+     * shortcut key to get it: sout + TAB
+     * 
+     * 
+     * Related Question:
+     * Print Below pattern:
+     *   *
+     *   **
+     *   ***
+     *   ****
+     */
+    System.out.println("My Output with 'println' function");
+    System.out.print("My Output with 'print' function 1 + ");
+    System.out.print("My Output with 'print' funtion 2\n");
+    System.out.print("My Output with 'print' funtion 3\n");
+    System.out.println("My Output with 'print' funtion 4");
+    System.out.println("\nRelated Question Solution:");
+    System.out.print("*\n");
+    System.out.print("**\n");
+    System.out.print("***\n");
+    System.out.print("****\n");
+    
+
+    // DEFINING VARIABLES
+    int a = 25;
+    int b = 10;
+    String name = "Tony Stark";
+    name = "Iron Man";
+    double c = a+b;
+    System.out.println(c);
+    System.out.println(name);
+
+    
+    // DEFINING DATATYPES
+    /*
+     * 8 Primitive: byte(8 bit/1bytes), short, char(2 bytes), boolean(1 byte), int(4 bytes), long(8 bytes), float(4 bytes), double(8 bytes).
+     * 5 Non-primitive: String, Array, Class, Object, Interface.
+     */
+
+     
+
+    // TAKING INPUT
+    }
+}
