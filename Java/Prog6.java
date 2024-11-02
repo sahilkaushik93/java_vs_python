77cd1e1d3a71752d9ae0ec8233c5c94015fc10e4
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-11-02 19:33:49 +0530

    Java Learning - Day 4 - 1

---
+++ Java/Prog6.java
@@ -0,0 +1,60 @@
+/*
+ * // PATTERN QUESTIONS
+ * 1. Butterfly Pattern => *      *
+ *      (for n = 4)        **    **
+ *                         ***  ***
+ *                         ********
+ *                         ********
+ *                         ***  ***
+ *                         **    **
+ *                         *      *
+ * 2. 
+ * 
+ */
+
+public class Prog6 {
+    public static void main(String[] args) {
+        // 1. BUTTERFLY PATTERN
+        /*
+         * Logic:
+         * row1: 1 star + 6 space + 1 star
+         * row2: 2 star + 4 space + 2 star
+         * row3: 3 star + 2 space + 3 star
+         * row4: 4 star + 0 space + 4 star
+         * row5: 4 star + 0 space + 4 star
+         * row6: 3 star + 2 space + 3 star
+         * row7: 2 star + 4 space + 2 star
+         * row8: 1 star + 6 space + 1 star
+         * 
+         * The pattern is repeating inversly after 4th row 
+         */
+
+        // First 4 rows:
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= (8-(2*i)); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Last 4 rows:
+        for(int i = 4; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= (8-(2*i)); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
